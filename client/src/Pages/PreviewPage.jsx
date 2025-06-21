d7242ef93f2bcf58b7ebf4fd8278ceb5702f5378
aadarsh-2004 <2020aadarshsoni@gmail>
2025-06-21 14:34:03 +0530

    added dependencies

--- client/src/Pages/PreviewPage.jsx
+++ client/src/Pages/PreviewPage.jsx
@@ -4,36 +4,36 @@ import { useUser } from "../context/UserContext";
 import { useEstimates } from "../context/EstimateContext";
 import axios from "axios";
 
-// Import themes
-import ThemeSimple from "../components/EstimateThemes/ThemeSimple";
-import ThemeElegant from "../components/EstimateThemes/ThemeElegant";
-import ThemeModern from "../components/EstimateThemes/ThemeModern";
+// // Import themes
+// import ThemeSimple from "../components/EstimateThemes/ThemeSimple";
+// import ThemeElegant from "../components/EstimateThemes/ThemeElegant";
+// import ThemeModern from "../components/EstimateThemes/ThemeModern";
 
-const PreviewEstimate = () => {
-  const { id } = useParams();
-  const {estimates} = useEstimates();
-  const { userData, loading } = useUser();
+ const PreviewEstimate = () => {
+//   const { id } = useParams();
+//   const {estimates} = useEstimates();
+//   const { userData, loading } = useUser();
 
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/estimates/get-by-id/${id}`)
-      .then(res => {
-        if (res.data.success) setEstimate(res.data.estimate);
-      });
-  }, [id]);
+//   useEffect(() => {
+//     axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/estimates/get-by-id/${id}`)
+//       .then(res => {
+//         if (res.data.success) setEstimate(res.data.estimate);
+//       });
+//   }, [id]);
 
-  if (!estimates || loading || !userData) return <div>Loading...</div>;
+//   if (!estimates || loading || !userData) return <div>Loading...</div>;
   
-  const selectedTheme = userData.selectedEstimateTheme ;
+//   const selectedTheme = userData.selectedEstimateTheme ;
   
-  const renderTheme = () => {
-    switch (selectedTheme) {
-      case "elegant": return <ThemeElegant estimate={estimates} studio={userData} />;
-      case "modern": return <ThemeModern estimate={estimates} studio={userData} />;
-      default: return <ThemeSimple estimate={estimates} studio={userData} />;
-    }
-  };
+//   const renderTheme = () => {
+//     switch (selectedTheme) {
+//       case "elegant": return <ThemeElegant estimate={estimates} studio={userData} />;
+//       case "modern": return <ThemeModern estimate={estimates} studio={userData} />;
+//       default: return <ThemeSimple estimate={estimates} studio={userData} />;
+//     }
+//   };
 
-  return <div className="min-h-screen bg-gray-100 p-6">{renderTheme()}</div>;
+//   return <div className="min-h-screen bg-gray-100 p-6">{renderTheme()}</div>;
 };
 
 export default PreviewEstimate;
